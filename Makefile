e4cc31e670abfaa94217b3f7a2a5bba0e5ee826c
vsoch <814322+vsoch@users.noreply.github.com>
2022-05-13 23:47:09 +0000

    Deploying to gh-pages from @ syspack/pakages@426e3b17fb8d894a13ea2a44cab49e12d7de1481 🚀

--- Makefile
+++
@@ -1,6 +0,0 @@
-SHELL = bash
-
-.PHONY: all
-
-all:
-	black pakages/*.py pakages/utils/*.py pakages/cli/*.py pakages/handlers/*.py
--- pakages/__init__.py
+++
@@ -1,6 +0,0 @@
-from pakages.version import __version__
-
-assert __version__
-import pakages.utils as utils
-
-utils.add_spack_to_path()
